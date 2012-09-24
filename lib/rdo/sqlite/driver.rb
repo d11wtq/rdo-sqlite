dd8dc935d4cbfab8bb2314185020d0b0139d7132
d11wtq <chris@w3style.co.uk>
2012-09-25 00:14:22 +1000

    Test basic opening of the database

--- lib/rdo/sqlite/driver.rb
+++ lib/rdo/sqlite/driver.rb
@@ -9,6 +9,11 @@ module RDO
   module SQLite
     # Main Driver class to hook into sqlite3 API
     class Driver < RDO::Driver
+      private
+
+      def filename
+        options[:path].to_s
+      end
     end
   end
 end
--- spec/sqlite/driver_spec.rb
+++ spec/sqlite/driver_spec.rb
@@ -1,7 +1,33 @@
 require "spec_helper"
+require "uri"
 
 describe RDO::SQLite::Driver do
-  it "does nothing" do
-    true.should be_false
+  let(:options)    { connection_uri }
+  let(:connection) { RDO.connect(options) }
+
+  after(:each) { connection.close rescue nil }
+
+  describe "#initialize" do
+    context "with valid settings" do
+      it "opens a database file" do
+        connection.should be_open
+      end
+    end
+
+    context "with invalid settings" do
+      let(:options) { "sqlite:/some/bad/path/to/a.db" }
+
+      it "raises an RDO::Exception" do
+        expect { connection }.to raise_error(RDO::Exception)
+      end
+
+      it "provides a meaninful error message" do
+        begin
+          connection && fail("RDO::Exception should be raised")
+        rescue RDO::Exception =>e
+          e.message.should =~ /sqlite.*file/i
+        end
+      end
+    end
   end
 end
